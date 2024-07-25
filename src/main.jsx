3b67c51c2fc647b57b6f456a01da6990720f1692
RaulxAG <raul.arrgon@outlook.es>
2024-07-25 17:34:50 +0200

    Second Commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './utils/i18n.jsx';
 import App from './App.jsx';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+import Inicio from './pages/Inicio.jsx';
+
+const router = createHashRouter([
+  {
+    path:"/",
+    element: <Inicio />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
