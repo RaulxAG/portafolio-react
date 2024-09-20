094aa7a88fa05be469584c3e818b92f4c45eaba5
RaulxAG <raul.arrgon@outlook.es>
2024-09-20 15:29:38 +0200

    added: hover effect projects cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,23 @@
 export default function ProjectCard({ t, project }) {
     return (
         <div className="projects__card rounded p-1 rounded-lg">
-            <img src={project.image} alt={project.title} className="w-100 object-cover mb-2 rounded" />
-            <h2 className="text-center font-bold text-lg">{project.title}</h2>
+            <figure className="card__image">
+                <img src={project.image} alt={project.title} className="w-100 object-cover mb-2 rounded" />
+            </figure>
 
-            {/* <p>{project.description}</p>
-            <div>
-                <strong>Lenguajes:</strong>
-                <ul>
-                    {project.languages.map((lang, index) => (
-                        <li key={index}>{lang}</li>
-                    ))}
-                </ul>
-            </div> */}
+            <article className="card__info">
+                <p>{project.description}</p>
+                <div>
+                    <strong>Lenguajes:</strong>
+                    <ul>
+                        {project.languages.map((lang, index) => (
+                            <li key={index}>{lang}</li>
+                        ))}
+                    </ul>
+                </div>
+            </article>
+
+            <h2 className="text-center font-bold text-lg">{project.title}</h2>
         </div>
     );
 }
