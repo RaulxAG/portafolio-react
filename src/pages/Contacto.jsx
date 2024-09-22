4b92a4d0bee120e790ad502990a107e219702a01
RaulxAG <raul.arrgon@outlook.es>
2024-09-22 15:41:58 +0200

    added: contact page responsive

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -4,40 +4,43 @@ export default function Contacto({ t }) {
   const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
   const onSubmit = (data) => {
+    let token = "";
+
     window.Email.send({
-      SecureToken: "your-secure-token", // Puedes generar un SecureToken desde https://smtpjs.com
+      SecureToken: token, // Puedes generar un SecureToken desde https://smtpjs.com
       To: 'raul.arrgon@outlook.es', // Cambia esto al correo donde quieres recibir los emails
-      From: data.email, // Correo del remitente (el usuario)
+      From: 'raul.arrgon@outlook.es', // Correo del remitente (el usuario)
       Subject: `Mensaje de ${data.name}`,
-      Body: data.message,
-    })
-    .then((message) => {
-      alert("Correo enviado exitosamente!");
-      reset(); // Resetear el formulario después de enviarlo
-    })
-    .catch((error) => alert("Error al enviar el correo: " + error));
+      Body: "Email:" + data.email + "<br>" +  "Nombre:" + data.name + "<br><br>" + "Mensaje:<br>" + data.message,
+    }).then(
+      message => alert(message)
+    ).catch((error) => alert("Error al enviar el correo: " + error));
   };
 
   return (
     <>
         <header className="pt-5">
-            <h1 className="py-4 text-center">{t('contact')}</h1>
+            <h1 className="pt-4 text-center">{t('contact')}</h1>
         </header>
 
-        <main className="d-flex align-items-center justify-content-center">
-            <form onSubmit={handleSubmit(onSubmit)} className="pt-5">
-                <div className="d-flex flex-column">
+        <main className="contact__content d-flex align-items-center justify-content-center m-auto">
+            <form onSubmit={handleSubmit(onSubmit)} className='w-100'>
+              
+              <div className='form__inputs d-flex align-items-center justify-content-center gap-4'>
+                <div className="d-flex flex-column w-50">
                     <label>Nombre<span className="text-danger">*</span></label>
                     <input
+                    placeholder={errors.name && errors.name.message}
                     type="text"
                     {...register('name', { required: "Este campo es requerido" })}
                     />
-                    {errors.name && <span className="text-warning">{errors.name.message}</span>}
+                    {/* {errors.name && <span className="text-warning">{errors.name.message}</span>} */}
                 </div>
                 
-                <div className="d-flex flex-column">
+                <div className="d-flex flex-column w-50">
                     <label>Email<span className="text-danger">*</span></label>
                     <input
+                    placeholder={errors.email && errors.email.message}
                     type="email"
                     {...register('email', {
                         required: "Este campo es requerido",
@@ -47,15 +50,18 @@ export default function Contacto({ t }) {
                         }
                     })}
                     />
-                    {errors.email && <span className="text-warning">{errors.email.message}</span>}
+                    {/* {errors.email && <span className="text-warning">{errors.email.message}</span>} */}
                 </div>
+              </div>
                 
                 <div className="d-flex flex-column">
                     <label>Mensaje<span className="text-danger">*</span></label>
-                    <textarea
+                    <textarea 
+                    placeholder={errors.message && errors.message.message}
+                    rows={7}
                     {...register('message', { required: "Este campo es requerido" })}
                     />
-                    {errors.message && <span className="text-warning">{errors.message.message}</span>}
+                    {/* {errors.message && <span className="text-warning">{errors.message.message}</span>} */}
                 </div>
                     
                 <button className="mt-2" type="submit">Enviar</button>
