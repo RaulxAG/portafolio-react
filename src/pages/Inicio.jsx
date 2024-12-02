43637c8f5e11a964fb1a51138e1da9d5384a1ac8
RaulxAG <raul.arrgon@outlook.es>
2024-12-02 13:16:47 +0100

    Added: Weather App Project

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,12 +1,15 @@
 import ProjectCard from "../components/ProjectCard"
 
 const projects = [
-  // { id: 1, title: "Fake Store", description: "Tienda Online haciendo uso de la API 'FakeStore'.", languages: ["javascript", "html5", "css3"], image: "assets/img/fakeStore.webp", link: "https://raulxag.github.io/fakeStore/" },
-  // { id: 2, title: "Portafolio Bento Grid", description: "Portafolio maquetado con bento grid estilo Apple.", languages: ["sass", "html5", "css3"], image: "assets/img/portafolio-sass.webp", link: "https://raulxag.github.io/portafolio/" },
-  { id: 3, title: "Monstedex", description: "Una enciclopedia de los monstruos del videojuego 'Monster Hunter'.", languages: ["vite", "react", "html5", "css3"], image: "assets/img/monstedex.webp", link: "https://raulxag.github.io/ReactApp/" },
-  // { id: 4, title: "Confeti Mario Block", description: "Web simple haciendo uso de una libreria para crear un efecto de confeti.", languages: ["javascript", "html5", "css3"], image: "assets/img/mario-block.webp", link: "https://raulxag.github.io/JS_ConfettiEffect/" },
-  // { id: 5, title: "Blog", description: "Un proyecto de blog simple proveniente del curso 'Master en CSS3' de Víctor Robles.", languages: ["css3", "html5"], image: "assets/img/blog.webp", link: "https://raulxag.github.io/blog/" },
-  { id: 6, title: "Reservas restaurante", description: "Simulación de web de reservas de un restaurante.", languages: ["sass", "html5", "css3"], image: "assets/img/restaurante.webp", link: "https://raulxag.github.io/proyecto-sass-restaurante/" },
+  { id: 1, title: "Weather App", description: "Consulta datos meteorológicos de tus zonas preferidas.", languages: ["vite", "react", "html5", "css3"], image: "assets/img/weather.webp", link: "https://raulxag-weather.vercel.app/" },
+  { id: 2, title: "Calculadora IMC", description: "Calcula tu índice de masa corporal de manera sencilla.", languages: ["html5", "css3", "javascript"], image: "assets/img/calculadora_imc.webp", link: "https://raulxag.github.io/calculadora_imc/" },
+  // { id: 3, title: "Fake Store", description: "Tienda Online haciendo uso de la API 'FakeStore'.", languages: ["javascript", "html5", "css3"], image: "assets/img/fakeStore.webp", link: "https://raulxag.github.io/fakeStore/" },
+  // { id: 4, title: "Portafolio Bento Grid", description: "Portafolio maquetado con bento grid estilo Apple.", languages: ["sass", "html5", "css3"], image: "assets/img/portafolio-sass.webp", link: "https://raulxag.github.io/portafolio/" },
+  // { id: 5, title: "Monstedex", description: "Una enciclopedia de los monstruos del videojuego 'Monster Hunter'.", languages: ["vite", "react", "html5", "css3"], image: "assets/img/monstedex.webp", link: "https://raulxag.github.io/ReactApp/" },
+  // { id: 6, title: "Confeti Mario Block", description: "Web simple haciendo uso de una libreria para crear un efecto de confeti.", languages: ["javascript", "html5", "css3"], image: "assets/img/mario-block.webp", link: "https://raulxag.github.io/JS_ConfettiEffect/" },
+  // { id: 6, title: "Blog", description: "Un proyecto de blog simple proveniente del curso 'Master en CSS3' de Víctor Robles.", languages: ["css3", "html5"], image: "assets/img/blog.webp", link: "https://raulxag.github.io/blog/" },
+  // { id: 7, title: "Reservas restaurante", description: "Simulación de web de reservas de un restaurante.", languages: ["sass", "html5", "css3"], image: "assets/img/restaurante.webp", link: "https://raulxag.github.io/proyecto-sass-restaurante/" },
+  
   // Agrega más proyectos aquí
 ];
 
